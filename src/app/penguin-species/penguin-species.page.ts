21bf3342764fdfac1d0e97f91402e297e0325a0c
Mina Moanes <menayou6@gmail.com>
2019-09-28 20:07:56 +0200

    Species page

--- src/app/penguin-species/penguin-species.page.ts
+++ src/app/penguin-species/penguin-species.page.ts
@@ -1,3 +1,4 @@
+import * as $ from 'jquery';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./penguin-species.page.scss'],
 })
 export class PenguinSpeciesPage implements OnInit {
+  species = [{
+    id: 1,
+    name: 'Penguin name',
+    img: 'africanpenguin.jpg',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.   Sunt ad laudantium ullam nobis magni, molestiae, nisi  doloribus fugiat quam, quo odio ex sequi eum recusandae  tempore optio! Veniam, mollitia soluta.'
+  }, {
+    id: 2,
+    name: 'Penguin name',
+    img: 'kingpenguin.jpg',
+
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.   Sunt ad laudantium ullam nobis magni, molestiae, nisi  doloribus fugiat quam, quo odio ex sequi eum recusandae  tempore optio! Veniam, mollitia soluta.'
+  }, {
+    id: 3,
+    name: 'html5',
+    img: 'emperorpenguin.jpg',
+
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.   Sunt ad laudantium ullam nobis magni, molestiae, nisi  doloribus fugiat quam, quo odio ex sequi eum recusandae  tempore optio! Veniam, mollitia soluta.'
+  }, {
+    id: 4,
+    name: 'js',
+    img: 'humboldtpenguin.jpg',
+
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.   Sunt ad laudantium ullam nobis magni, molestiae, nisi  doloribus fugiat quam, quo odio ex sequi eum recusandae  tempore optio! Veniam, mollitia soluta.'
+  }, {
+    id: 5,
+    name: 'js',
+    img: 'southernrockhopperpenguin.jpg',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.   Sunt ad laudantium ullam nobis magni, molestiae, nisi  doloribus fugiat quam, quo odio ex sequi eum recusandae  tempore optio! Veniam, mollitia soluta.'
+  }];
+  expanded = false;
+  emptySpecie = {
+    id: 0,
+    name: '',
+    img: '',
+    desc: ''
+  };
+  selectedSpecie = {};
 
   constructor() { }
 
   ngOnInit() {
+    this.selectedSpecie = this.emptySpecie;
   }
 
+  goBack() {
+    this.selectedSpecie = this.emptySpecie;
+  }
+  showAll(evt) {
+    this.expanded = !this.expanded;
+  }
+  showPenguin(evt, id) {
+    this.selectedSpecie = this.species.find(x => x.id === id);
+    // this.getContent(this);
+  }
 }
