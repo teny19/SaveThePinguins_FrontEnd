95ad69298365a02b97937e0386b5f7332057d770
Mina Moanes <menayou6@gmail.com>
2019-09-28 20:37:00 +0200

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
 })
+
+
 export class AppComponent {
+  currentPage = location.pathname.split('/')[1];
   constructor(
+    private router: Router,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 
 @Component({
@@ -14,19 +13,21 @@ export class WelcomePage {
     slidesPerView: 1,
     pager: true
   };
+  ngOnInit() {
+  }
 
   penguins = [
-  	{'name': 'Bruno', 'age': '7', 'img': 'penguinbruno.png'},
-  	{'name': 'Franziska', 'age': '5', 'img': 'penguinfranziska.png'},
-  	{'name': 'Julia', 'age': '2', 'img': 'penguinjulia.png'},
-  	{'name': 'Ferdinand', 'age': '10', 'img': 'penguinferdinand.png'}
+    { 'name': 'Bruno', 'age': '7', 'img': 'penguinbruno.png' },
+    { 'name': 'Franziska', 'age': '5', 'img': 'penguinfranziska.png' },
+    { 'name': 'Julia', 'age': '2', 'img': 'penguinjulia.png' },
+    { 'name': 'Ferdinand', 'age': '10', 'img': 'penguinferdinand.png' }
   ]
 
   constructor() {
-  	// this.router.navigateByUrl('/tabs/');
+    // this.router.navigateByUrl('/tabs/');
   }
 
-  move(slides, page, penguin){
+  move(slides, page, penguin) {
     slides.slideTo(page);
     console.log('penguin', penguin);
   }
