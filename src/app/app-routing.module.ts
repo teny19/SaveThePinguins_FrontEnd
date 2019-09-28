3ce5894e451420c1f6b18acd59ad81ed1cbe98d5
Teny <tenyang.rabgang@gmail.com>
2019-09-28 03:22:45 +0200

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: './welcome/welcome.module#WelcomePageModule' },
+  { path: 'tabs', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule) },
+  // { path: 'tabs/tab1', loadChildren: () => import('./tab1/tab1.module').then(m => m.Tab1PageModule) },
+  { path: 'messaging', loadChildren: './messaging/messaging.module#MessagingPageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
